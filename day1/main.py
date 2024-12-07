6ccdfc8f5ea41889bd21e0e019add33ca51190ae
MuslicaCrew <360bruno360@gmail.com>
2024-12-08 00:39:23 +0100

    Added first part of the problem

--- day1/main.py
+++ day1/main.py
@@ -12,12 +12,35 @@ except IOError:
     print("An error occurred while reading the file.")
 # print(input)
 
+# TESTING ONLY
+
 #example = """3   4
 #4   3
 #2   5
 #1   3
 #3   9
 #3   3""".splitlines()
+
+# PART 1
+print("Answer for part 1:")
+left = list()
+right = list()
+for line in input:
+    left.append(line.split(" ")[0])
+    right.append(line.split(" ")[3])
+left.sort()
+right.sort()
+
+ans = 0
+
+for i, _ in enumerate(left):
+    ans += abs(int(left[i]) - int(right[i]))
+
+print(ans)
+
+
+# PART 2
+print("Answer for part 2:")
 left = list()
 right = list()
 for line in input:
